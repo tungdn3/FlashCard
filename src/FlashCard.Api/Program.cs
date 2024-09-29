ec17c3737b6f9517540a368e64d5b5d68a0b5aee
Tung Nguyen <tnguyen52@mantu.com>
2024-09-29 17:10:39 +0700

    Add Duende IdentityServer quick start

--- src/FlashCard.Api/Program.cs
+++ src/FlashCard.Api/Program.cs
@@ -1,6 +1,8 @@
 using FlashCard.Core;
 using FlashCard.Infrastructure;
 using FlashCard.Infrastructure.EF;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -18,7 +20,60 @@ builder.Services.AddSwaggerGen(options =>
         Title = "Flash Card API",
         Description = "An ASP.NET Core Web API for managing flash cards",
     });
+
+    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        Type = SecuritySchemeType.OAuth2,
+        Flows = new OpenApiOAuthFlows
+        {
+            Implicit = new OpenApiOAuthFlow
+            {
+                AuthorizationUrl = new Uri($"{builder.Configuration["Auth:Authority"]}/connect/authorize"),
+                TokenUrl = new Uri($"{builder.Configuration["Auth:Authority"]}/connect/token"),
+                Scopes = new Dictionary<string, string>
+                {
+                    { "flash-card-api", "Flash Card API - full access" }
+                }
+            }
+        },
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Scheme = JwtBearerDefaults.AuthenticationScheme,
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "oauth2",
+                },
+            },
+            Array.Empty<string>()
+        }
+    });
 });
+
+builder.Services
+    .AddAuthentication()
+    .AddJwtBearer(options =>
+    {
+        options.Authority = builder.Configuration["Auth:Authority"];
+        options.TokenValidationParameters.ValidateAudience = false;
+    });
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("ApiScope", policy =>
+    {
+        policy.RequireAuthenticatedUser();
+        policy.RequireClaim("scope", "flash-card-api");
+    });
+});
+
 builder.Services.AddCoreLayer();
 builder.Services.AddInfrastructureLayer(builder.Configuration);
 
@@ -28,7 +83,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.OAuthClientId(app.Configuration["Auth:SwaggerClientId"]);
+    });
+
     app.UseExceptionHandler("/error-development");
 }
 else
---
+++ src/FlashCard.Auth/Config.cs
@@ -0,0 +1,60 @@
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+
+namespace FlashCard.Auth;
+
+public static class Config
+{
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new IdentityResource[]
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile(),
+        };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new ApiScope[]
+        {
+            new ApiScope(name: "flash-card-api", displayName: "Flash Card API")
+        };
+
+    public static IEnumerable<Client> Clients =>
+        new Client[]
+        {
+            new Client
+            {
+                ClientId = "api-swagger",
+                AllowedGrantTypes = GrantTypes.Implicit,
+                RequireClientSecret = false,
+                RequirePkce = false,
+                AllowAccessTokensViaBrowser = true,
+                RedirectUris = { "https://localhost:7002/swagger/oauth2-redirect.html" },
+                AllowedScopes =
+                {
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile,
+                    "flash-card-api"
+                }
+            },
+            new Client
+            {
+                ClientId = "web",
+                ClientSecrets = { new Secret("secret".Sha256()) },
+
+                AllowedGrantTypes = GrantTypes.Code,
+            
+                // where to redirect to after login
+                RedirectUris = { "https://localhost:5002/signin-oidc" },
+
+                // where to redirect to after logout
+                PostLogoutRedirectUris = { "https://localhost:5002/signout-callback-oidc" },
+
+                AllowedScopes =
+                {
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile,
+                    "flash-card-api"
+                }
+            }
+        };
+}
